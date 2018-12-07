dcf31d3103d767afe1c61660b96c53010fabff96
unknown <petri.kalima@citrus.fi>
2018-12-07 17:19:20 +0200

    fix

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import oandapyV20 as opy
+from configparser import ConfigParser
 import pandas as pd
 import oandapyV20.endpoints.trades as trades
 from oandapyV20.contrib.requests import MarketOrderRequest
@@ -6,10 +7,11 @@ from oandapyV20.contrib.requests import TakeProfitDetails, StopLossDetails
 import oandapyV20.endpoints.orders as orders
 import json
 
-ACCESS_TOKEN = 'c36b759ab5a83d58ca91b5ee1bd8aee1-660f3e0b44165ffca1d2cb6bebf2c495'
-ACCOUNT_ID = '101-004-9917560-001'
 
 def main():
+    params = config()
+    ACCESS_TOKEN = params[0]
+    ACCOUNT_ID = params[1]
     client = opy.API(environment='practice', access_token=ACCESS_TOKEN)
     EUR_USD_STOP_LOSS = 1.10
     EUR_USD_TAKE_PROFIT = 1.16
@@ -28,7 +30,20 @@ def main():
     else:
         print(json.dumps(rv, indent=2))
 
-                           
+
+
+def config(filename='values.config', section='Forex'):
+    parser = ConfigParser()
+    parser.read(filename)
+    configParams = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            configParams[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+ 
+    return configParams                           
 
 
 def getTrades():
